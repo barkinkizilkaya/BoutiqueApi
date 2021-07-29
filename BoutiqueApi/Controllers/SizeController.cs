1cf9b402ef35114cb09f670664cfb6c860cc4608
barkinkizilkaya <barkin.kizilkaya@outlook.com>
2021-07-30 00:16:06 +0300

    size controler actions complated

--- BoutiqueApi/Controllers/SizeController.cs
+++ BoutiqueApi/Controllers/SizeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using BoutiqueApi.Data;
 using BoutiqueApi.IRepositories;
 using BoutiqueApi.Models;
 using Microsoft.AspNetCore.Http;
@@ -39,5 +40,82 @@ namespace BoutiqueApi.Controllers
             }
         }
 
-    }
+        [HttpPost]
+        public async Task<IActionResult> CreateSize([FromBody] SizeDTO sizeDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var size = _mapper.Map<Size>(sizeDTO);
+                await _sizeRepository.Insert(size);
+                return CreatedAtRoute("GetAllSizes", new { ProductId = size.ProductId, size });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "InternalServer Error. Please Try Again Later");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSize([FromBody] SizeDTO sizeDTO)
+        {
+
+            if(!ModelState.IsValid || sizeDTO.Id <1)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+
+                var size = await _sizeRepository.Get(sizeDTO.Id);
+                if(size == null)
+                {
+                    return BadRequest("Submited data is Invalid");
+                }
+
+                _mapper.Map(sizeDTO, size);
+                _sizeRepository.Update(size);
+
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Try Again Later");
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSize(int Id)
+        {
+            if(Id<1)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var size = await _sizeRepository.Get(Id);
+
+                if(size == null)
+                {
+                    return BadRequest("Submited Data Invalid");
+
+                }
+
+                await _sizeRepository.Delete(Id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error, Please Try Again Later");
+            }
+
+        }
+        }
 }
